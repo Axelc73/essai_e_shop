e88cc2176b989983816c3bf8b53634ae21cffe93
Axelc73 <acristolo@outlook.fr>
2025-03-26 18:54:37 +0100

    first commit

---
+++ store/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from store.models import Product
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'store/index.html', context={"products": products})
+
+def detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {'products': product}
+    return render(request, 'store/detail.html', context)
